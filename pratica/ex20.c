15b36efd0c7eb52ba5f68093b1d6562f4ec5d418
LucasManera2017 <lucas.manera2017@gmail.com>
2025-03-26 19:15:39 -0300

    Finalizado exercicio 20

--- pratica/ex20.c
+++ pratica/ex20.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  int a,b,c,d,e,f,g,h,i,j;  
-  float media, num;
+  int a,b,c,d,e,f,g,h,i,j, media, contador;  
 
   printf("Digite um numero : \n");
   scanf("%d",&a);
@@ -26,16 +25,50 @@ int main() {
   scanf("%d",&j);
 
   //Validar se é um número par
-  if(a%2!=0) media = a;
-  if(b%2!=0) media = b+1;
-  if(c%2!=0) c = c+1;
-  if(d%2!=0) d = d+1;
-  if(e%2!=0) e = e+1;
-  if(f%2!=0) f = f+1;
-  if(g%2!=0) g = g+1;
-  if(h%2!=0) h = h+1;
-  if(i%2!=0) i = i+1;
-  if(j%2!=0) j = j+1;
+  if(a%2==0){
+    contador++;
+    media = a;
+  } 
+  if(b%2==0) {
+    contador++; 
+    media += b;
+  } 
+  if(c%2==0) {
+    contador++; 
+    media += c;
+  } 
+  if(d%2==0) {
+    contador++; 
+    media += d;
+  }
+  if(e%2==0) {
+    contador++; 
+    media += e;
+  }
+  if(f%2==0) {
+    contador++; 
+    media += f;
+  }
+  if(g%2==0) {
+    contador++; 
+    media += g;
+  }
+  if(h%2==0) {
+    contador++; 
+    media += h;
+  }
+  if(i%2==0) {
+    contador++; 
+    media += i;
+  }
+  if(j%2==0) {
+    contador++; 
+    media += j;
+  }
+
+  int mediaTotal = media/contador; 
+
+  printf("A media dos numeros pares e: %d \n", mediaTotal);
 
 
 

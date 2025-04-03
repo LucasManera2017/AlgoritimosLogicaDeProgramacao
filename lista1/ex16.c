b1314409af1757ab2ccafa6b61c114b503ab59fa
LucasManera2017 <lucas.manera2017@gmail.com>
2025-04-02 22:24:50 -0300

    Ultimas correcoes

--- lista1/ex16.c
+++ lista1/ex16.c
@@ -6,8 +6,12 @@ int main (void){
   printf("Digite o ano: \n");
   scanf("%d", &ano);
 
-  if (ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0) {
-    printf("%d e um ano bissexto.\n", ano);
+  if (ano % 4 == 0 ) {
+    if (ano % 100 == 0 && ano % 400 == 0){
+      printf("%d nao e um ano bissexto.\n", ano);
+    } else {
+      printf("%d e um ano bissexto.\n", ano);
+    }
   } else {
     printf("%d nao e um ano bissexto.\n", ano);
   }
